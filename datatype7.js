3954780565246bfba2851edd4bfd195a9e81031d
rajput-13 <thakurditto@gmail.com>
2024-04-15 19:03:35 +0530

    master: added types string numbers

---
+++ datatype7.js
@@ -0,0 +1,12 @@
+console.log('this is my learnig javascript')
+dell = 500
+console.log(typeof dell)
+
+// वेरीअबल लेते है isvaccinated  boolean see also loops 
+isvaccinated = true 
+console.log(typeof isvaccinated)
+console.log(typeof false)
+
+// string type 
+name = "vikram"
+console.log(typeof name)
